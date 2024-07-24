d29c59c13ed6780598d0e8c0a0a75f5d677d023d
PurpleFishh <asmarandei.catalin@yahoo.com>
2024-07-24 11:32:32 +0300

    work[auth]: Validation and messages for each validation

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -15,15 +15,28 @@ import { AuthenticationService } from 'src/app/core/services/authentication.serv
 })
 export class LoginComponent {
   hide = true;
+  firstSubmited = false;
   formValues: unknown;
   form: FormGroup = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
+    email: new FormControl('', [
+      Validators.required,
+      //Validators.email
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      //Validators.minLength(6),
+      //Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+$'),
+    ]),
   });
 
- constructor(private readonly authService: AuthenticationService) {}
+  constructor(private readonly authService: AuthenticationService) {}
+
+  ngOnInit(): void {
+    this.firstSubmited = false;
+  }
 
   onSubmit() {
+    this.firstSubmited = true;
     let loginDo: LoginDto = this.form.getRawValue();
     console.log(loginDo);
     this.authService.login(loginDo);
