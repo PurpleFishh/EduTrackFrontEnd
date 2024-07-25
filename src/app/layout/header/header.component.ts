a0f5ef08b85513291c19519a3f33ffae7ac16e6c
PurpleFishh <asmarandei.catalin@yahoo.com>
2024-07-26 02:02:59 +0300

    work[navbar]: added primary color for button of the page that is currently displayed

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,24 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  constructor(private readonly router: Router) {}
-  
+  currentRoute: string = '';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.currentRoute = this.router.url;
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.urlAfterRedirects;
+      }
+    });
+  }
+
   navigate(path: string) {
     this.router.navigateByUrl(path);
+  }
+
+  isCurrentPage(route: string): boolean {
+    return this.currentRoute === route;
   }
 }
