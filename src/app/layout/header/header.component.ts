d5a3e0155063c4fed3a05ff2eb9c27c772178598
PurpleFishh <asmarandei.catalin@yahoo.com>
2024-07-26 15:33:00 +0300

    work[coruses]: made the courses filtering, sorting, categorie filter

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup, NgForm } from '@angular/forms';
 import { NavigationEnd, Router } from '@angular/router';
+import { CoursesService } from 'src/app/core/services/courses.service';
 
 @Component({
   selector: 'app-header',
@@ -9,14 +11,13 @@ import { NavigationEnd, Router } from '@angular/router';
 export class HeaderComponent {
   currentRoute: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private readonly router: Router, private readonly coursesService: CoursesService) {}
 
   ngOnInit() {
-    this.currentRoute = this.router.url;
+    this.currentRoute = "/"+ this.router.url.split('?')[0].split('/')[1];
     this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        this.currentRoute = event.urlAfterRedirects;
-      }
+      if (event instanceof NavigationEnd)
+        this.currentRoute = "/"+ event.urlAfterRedirects.split('?')[0].split('/')[1];
     });
   }
 
@@ -26,5 +27,13 @@ export class HeaderComponent {
 
   isCurrentPage(route: string): boolean {
     return this.currentRoute === route;
+  }
+
+  onSearchSubmit(form: NgForm) {
+    if(form.value.search != '') {
+      this.router.navigateByUrl(`/courses?search=${form.value.search}`);
+    }else{
+      this.router.navigateByUrl(`/courses`);
+    }
   }
 }
--- src/app/pages/courses/courses.component.ts
+++ src/app/pages/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { CourseDisplayDto } from 'src/app/core/models/course.model';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { CourseDisplayDto, CoursesFilter, CoursesFilterDto } from 'src/app/core/models/course.model';
 import { CoursesService } from 'src/app/core/services/courses.service';
 
 @Component({
@@ -8,15 +9,107 @@ import { CoursesService } from 'src/app/core/services/courses.service';
   styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent {
-  courses: CourseDisplayDto[] = []
+  search: string = '';
+  sortBy: string = 'relv';
+  difficulty: string = 'all';
+  categorySelected: string = '';
+  prerequisites: { [key: string]: boolean } = {};
 
-  constructor(private readonly coursesService: CoursesService) {}
+  courses: CourseDisplayDto[] = []
+  filtersAvailable: CoursesFilterDto | undefined;
+
+  constructor(private readonly coursesService: CoursesService,
+     private readonly routerActive: ActivatedRoute,
+     private readonly router: Router
+    ) {}
 
   ngOnInit(){
-    this.coursesService.getCourses().subscribe(courses => {
-      console.log(courses);
-      this.courses = courses
+    this.routerActive.queryParams.subscribe(params => {
+      this.search = params['search'];
+      if(params['sort'])
+        this.sortBy = params['sort'];
+      else
+        this.sortBy = 'relv';
+      this.categorySelected = params['category'];
+      if(params['difficulty'])
+        this.difficulty = params['difficulty'];
+      else
+        this.difficulty = 'all';
+      let prereqFiltrt = [];
+      if(params['prerequisites'])
+      {
+        prereqFiltrt = params['prerequisites'].split(',');
+        this.prerequisites = prereqFiltrt.reduce((acc: { [x: string]: boolean; }, value: string | number) => {
+          acc[value] = true;
+          return acc;
+        }, {} as { [key: string]: boolean });
+      }
+      else
+        this.prerequisites = {};
+
+      let filter: CoursesFilter = {
+          search: this.search,
+          sortBy: this.sortBy === 'relv' ? '' : this.sortBy,
+          categories: this.categorySelected,
+          difficulties: this.difficulty === 'all' ? '' : this.difficulty,
+          prerequistes: prereqFiltrt
+      }
+      this.coursesService.getCourses(filter).subscribe(courses => {
+        console.log(courses);
+        this.courses = courses
+      });
+      this.coursesService.getFilters(filter).subscribe(filters => {
+        this.filtersAvailable = filters
+        console.log(this.filtersAvailable);
+      });
     });
+  }
+
+  difficultySelected(difficulty: string) {
+    this.difficulty = difficulty;
+    this.prerequisites = {};
+
+    this.filterCourses();  
+  }
+
+  sortBySelected(sort: string) {
+    this.sortBy = sort;
+
+    this.filterCourses();  
+  }
+
+  categorySelect(category: string) {
+    this.categorySelected = category;
+    this.difficulty = 'all';
+    this.prerequisites = {};
+
+    this.filterCourses();
+  }
+
+  onPrerequisitesChange(prereq: string, selected: boolean) {
+    this.prerequisites[prereq] = selected;
+    this.filterCourses();
+  }
+
+  filterCourses() {
+    let url = `courses`;
+    let querys: string[] = [];
+    if(this.search)
+      querys.push(`search=${this.search}`)
+    if(this.sortBy)
+      querys.push(`sort=${this.sortBy}`)
+    if(this.categorySelected)
+      querys.push(`category=${this.categorySelected}`)
+    if(this.difficulty !== 'all')
+      querys.push(`difficulty=${this.difficulty}`)
+
+    let prereqs = Object.keys(this.prerequisites).filter(key => this.prerequisites[key]);
+    if(prereqs.length > 0)
+      querys.push(`prerequisites=${prereqs.join(',')}`)
+
+    url += `?${querys.join('&')}`;
+    console.log(url);
+    this.router.navigateByUrl(url);
   }
 
   courses2 = [
