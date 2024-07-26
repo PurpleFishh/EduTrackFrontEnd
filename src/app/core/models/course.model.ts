147cd3ddc7ebca9972f1296c817bd84eca98af71
PurpleFishh <asmarandei.catalin@yahoo.com>
2024-07-26 15:33:00 +0300

    work[coruses]: made the courses filtering, sorting, categorie filter

--- src/app/core/models/course.model.ts
+++ src/app/core/models/course.model.ts
@@ -5,3 +5,16 @@ export interface CourseDisplayDto {
     shortDescription: string;
     imageContents: string;
 }
+export interface CoursesFilterDto {
+    categories: string[];
+    difficulties: string[];
+    prerequistes: string[];
+}
+export interface CoursesFilter {
+    search: string;
+    sortBy: string;
+    categories: string;
+    difficulties: string;
+    prerequistes: string[];
+}
+
--- src/app/core/services/courses.service.ts
+++ src/app/core/services/courses.service.ts
@@ -1,17 +1,48 @@
 import { Injectable } from '@angular/core';
 import { RestBaseService } from './rest-base.service';
-import { CourseDisplayDto } from '../models/course.model';
+import {
+  CourseDisplayDto,
+  CoursesFilter,
+  CoursesFilterDto,
+} from '../models/course.model';
+import { query } from '@angular/animations';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoursesService {
   readonly endpoint: string = 'Course';
 
-  constructor(private readonly baseService: RestBaseService) { }
+  constructor(private readonly baseService: RestBaseService) {}
 
-  getCourses() {
-    console.log('getCourses');
-    return this.baseService.get<CourseDisplayDto[]>(`${this.endpoint}/GetAllCourses`);
+  getCourses(filter: CoursesFilter) {
+    let url = this.getUrlForFilter(`${this.endpoint}/GetAllCourses`, filter);
+    return this.baseService.get<CourseDisplayDto[]>(url);
+  }
+
+  getFilters(filter: CoursesFilter) {
+    let url = this.getUrlForFilter(`${this.endpoint}/GetFilters`, filter);
+    return this.baseService.get<CoursesFilterDto>(url);
+  }
+
+  private getUrlForFilter(baseUrl: string, filter: CoursesFilter) {
+    let querys: string[] = [];
+    if (filter.search) querys.push(`Title=${filter.search}`);
+    if (filter.sortBy) querys.push(`SortBy=${filter.sortBy}`);
+    if (filter.difficulties) querys.push(`Difficulties=${filter.difficulties}`);
+    if (filter.categories) querys.push(`Categories=${filter.categories}`);
+    if (filter.prerequistes.length > 0) {
+      filter.prerequistes.forEach((p) => {
+        querys.push(`Prerequistes=${p}`);
+      });
+    }
+    baseUrl += `?${querys.join('&')}`;
+    return baseUrl;
+  }
+
+  searchCourses(search: string) {
+    return this.baseService.get<CourseDisplayDto[]>(
+      `${this.endpoint}/GetAllCourses/?title=${search}`
+    );
   }
 }
