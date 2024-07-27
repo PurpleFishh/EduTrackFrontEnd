0bede1949c23f25c8c92f259650e2d6bb52127e2
PurpleFishh <asmarandei.catalin@yahoo.com>
2024-07-27 16:50:17 +0300

    work[corse]: Made the page for course disply, without binding

---
+++ src/app/pages/course-details/course-action-area/course-action-area.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-action-area',
+  templateUrl: './course-action-area.component.html',
+  styleUrls: ['./course-action-area.component.scss']
+})
+export class CourseActionAreaComponent {
+
+}
