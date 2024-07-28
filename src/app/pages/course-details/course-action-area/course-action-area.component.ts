c11f50b51fb4796a241805f5dfa61887a12a87b3
PurpleFishh <asmarandei.catalin@yahoo.com>
2024-07-28 12:33:13 +0300

    work[course]: Added operation over the course for the teacher owner of the course

--- src/app/pages/course-details/course-action-area/course-action-area.component.ts
+++ src/app/pages/course-details/course-action-area/course-action-area.component.ts
@@ -1,5 +1,6 @@
 import { AfterContentInit, Component, Input, OnInit } from '@angular/core';
 import { CourseDto } from 'src/app/core/models/course.model';
+import { AuthenticationService } from 'src/app/core/services/authentication.service';
 import { FileReaderService } from 'src/app/core/services/file-reader.service';
 
 @Component({
@@ -11,13 +12,16 @@ export class CourseActionAreaComponent implements  AfterContentInit  {
   @Input({ required: true }) course!: CourseDto;
   minutesDuration: number = 0;
   hoursDuration: number = 0;
+  isTeacherOwner = false;
 
-  constructor(private readonly fileReader: FileReaderService) {}
+
+  constructor(private readonly fileReader: FileReaderService, private readonly auth: AuthenticationService) {}
 
   ngAfterContentInit() {
     this.hoursDuration = Math.floor(this.course.duration / 60);
     this.minutesDuration = this.course.duration % 60;
 
     this.course.imageContents = this.fileReader.readImage(this.course.imageContents);
+    this.isTeacherOwner = this.auth.isTeacher() && this.auth.getEmail() === this.course.teacherEmail;
   }
 }
--- src/app/pages/course-details/course-details.component.ts
+++ src/app/pages/course-details/course-details.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CourseDisplayDto, CourseDto, CoursesFilter, CoursesFilterDto } from 'src/app/core/models/course.model';
 import { LessonDisplayDto } from 'src/app/core/models/lesson.model';
+import { AuthenticationService } from 'src/app/core/services/authentication.service';
 import { CoursesService } from 'src/app/core/services/courses.service';
 import { LessonsService } from 'src/app/core/services/lessons.service';
 
@@ -16,6 +17,9 @@ export class CourseDetailsComponent {
   realtedCourses: CourseDisplayDto[] = [];
   lessons: LessonDisplayDto[] = [];
   descriptionExpanded = false;
+  actionAreaHeight = 0;
+  descriptionOverflow = true;
+  
 
   constructor(
     private readonly route: ActivatedRoute,
@@ -28,6 +32,14 @@ export class CourseDetailsComponent {
       this.courseId = params['id'];
       this.courseService.getCourse(this.courseId).subscribe((course) => {
         this.course = course;
+        if(document.getElementById('action-area'))
+          this.actionAreaHeight = document.getElementById('action-area')!.offsetHeight;
+        if(document.getElementById('description-text'))
+        {  
+          let descriptionText = document.getElementById('description-text')!;
+          console.log(descriptionText, descriptionText.offsetHeight, descriptionText.scrollHeight);
+          this.descriptionOverflow = descriptionText.offsetHeight  < descriptionText.scrollHeight;
+        }
 
         let filter: CoursesFilter = {
           search: '',
@@ -38,9 +50,6 @@ export class CourseDetailsComponent {
         }
         this.courseService.getCourses(filter).subscribe((related) => {
           this.realtedCourses = related.filter((course) => course.name !== this.course.name).slice(0, 5);
-        });
-        this.courseService.getStudentEnrolled(this.courseId).subscribe((stud) => {
-          console.log(stud);
         });
       });
       this.lessonService.getAllLessons(this.courseId).subscribe((lessons) => {
