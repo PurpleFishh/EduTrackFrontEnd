fff385f4fdff9e7eff23107353fe8d56dcaef573
PurpleFishh <asmarandei.catalin@yahoo.com>
2024-07-30 01:21:40 +0300

    work[dashboard]: Finished the main page for student dashboard doing the rest calls to backend

--- src/app/pages/dashboard/components/legend/legend.component.ts
+++ src/app/pages/dashboard/components/legend/legend.component.ts
@@ -1,18 +1,18 @@
-import { AfterContentInit, Component, Input } from '@angular/core';
+import { AfterContentInit, Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-legend',
   templateUrl: './legend.component.html',
   styleUrls: ['./legend.component.scss']
 })
-export class LegendComponent implements  AfterContentInit {
+export class LegendComponent implements  OnChanges {
   @Input() completeText: string | undefined;
   @Input() incompleteText: string | undefined;
   @Input() complete: number = 0;
   @Input() incomplete: number = 0;
-  procentage = 0;
+  procentage: number = 0;
 
-  ngAfterContentInit(){
+  ngOnChanges(){
     this.procentage = Math.round((this.complete / (this.complete + this.incomplete)) * 100);
   }
 
