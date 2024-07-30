44d1b3df76130857845e86196516b3efebc414e3
PurpleFishh <asmarandei.catalin@yahoo.com>
2024-07-30 22:35:34 +0300

    work[courses]: Integrated the lesson start date into courses page

--- src/app/core/models/lesson.model.ts
+++ src/app/core/models/lesson.model.ts
@@ -2,11 +2,13 @@ export interface LessonDto {
     name: string;
     description: string;
     lesson_Content: string;
+    startDate: Date;
 }
 
 export interface LessonDisplayDto {
     name: string,
     description: string,
     lesson_Content: string,
-    lessonStatus: string
+    lessonStatus: string,
+    startDate: Date;
 }
