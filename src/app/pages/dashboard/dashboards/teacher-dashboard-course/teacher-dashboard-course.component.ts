10f11386d55211f210cba663184995399f3331a4
PurpleFishh <asmarandei.catalin@yahoo.com>
2024-08-01 21:23:20 +0300

    work[dashboard]: Integrated teacher my courses dashboard

--- src/app/pages/dashboard/dashboards/teacher-dashboard-course/teacher-dashboard-course.component.ts
+++ src/app/pages/dashboard/dashboards/teacher-dashboard-course/teacher-dashboard-course.component.ts
@@ -3,10 +3,11 @@ import { CoursesService } from 'src/app/core/services/courses.service';
 import { LessonsService } from 'src/app/core/services/lessons.service';
 import { AuthenticationService } from 'src/app/core/services/authentication.service';
 import { AssignmentInventoryService } from 'src/app/core/services/assignment-inventory-service.service';
-import { AssignmentDisplayDto, Grade } from 'src/app/core/models/assignment.model';
+import { AllAssignments, AssignmentDisplayDto, Grade, Student } from 'src/app/core/models/assignment.model';
 import { LessonDisplayDto } from 'src/app/core/models/lesson.model';
 import { StudentDto } from 'src/app/core/models/student.model';
 import { AttendanceDictionary } from 'src/app/core/models/attendance.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-teacher-dashboard-course',
@@ -16,7 +17,7 @@ import { AttendanceDictionary } from 'src/app/core/models/attendance.model';
 export class TeacherDashboardCourseComponent implements OnInit {
   courses: string[] = [];
   firstCourseTitle: string = '';
-  firstLessonTitle: string = ''; // Add this line
+  firstLessonTitle: string = ''; 
   selectedCourse: string | null = null;
   selectedLesson: string | null = null;
   lessons: LessonDisplayDto[] = [];
@@ -28,15 +29,19 @@ export class TeacherDashboardCourseComponent implements OnInit {
   displayedColumns: string[] = ['name', 'email', 'status'];
   
   // New properties for assignments
-  assignments: AssignmentDisplayDto[] = [];
+  //assignments: AssignmentDisplayDto[] = [];
+  assignments: AllAssignments[] = [];
   displayedColumnsAssignments: string[] = ['assignmentName', 'status', 'grade'];
-  assignmentGrades: { [key: string]: { status: string, grade: number } } = {};
+  //assignmentGrades: { [key: string]: { status: string, grade: number } } = {};
+
+  
 
   constructor(
     private coursesService: CoursesService,
     private lessonsService: LessonsService,
     private authService: AuthenticationService,
-    private assignmentService: AssignmentInventoryService
+    private assignmentService: AssignmentInventoryService,
+    private readonly router: Router
   ) {}
 
   ngOnInit(): void {
@@ -50,6 +55,7 @@ export class TeacherDashboardCourseComponent implements OnInit {
         this.courses = data;
         if (this.courses.length > 0) {
           this.firstCourseTitle = this.courses[0];
+          this.selectedCourse = this.firstCourseTitle;
         } else {
           this.firstCourseTitle = 'No courses available';
         }
@@ -81,7 +87,7 @@ export class TeacherDashboardCourseComponent implements OnInit {
         this.lessons = lessons;
         if (this.lessons.length > 0) {
           this.firstLessonTitle = this.lessons[0].name;
-          this.selectedLesson = null; // Reset selected lesson when fetching new lessons
+          this.selectedLesson = this.firstLessonTitle; 
         }
       },
       error: (error) => {
@@ -124,17 +130,17 @@ export class TeacherDashboardCourseComponent implements OnInit {
 
   // New methods for assignments
   fetchAssignments(course: string, lesson: string): void {
-    this.assignmentService.getAssignment(course, lesson).subscribe({
-      next: (assignments: AssignmentDisplayDto[]) => {
-        this.assignments = assignments;
-        this.assignmentGrades = {}; // Clear previous assignment grades
-        if (assignments.length > 0) {
-          this.studentsEnrolled.forEach(student => {
-            assignments.forEach(assignment => {
-              this.fetchAssignmentGrade(course, lesson, student.email, assignment.assignment_name);
-            });
-          });
-        }
+    this.assignmentService.getAllAssignmentsSent(course, lesson).subscribe({
+      next: (assignments: AllAssignments[][]) => {
+        this.assignments = assignments.flat();
+        //this.assignmentGrades = {}; // Clear previous assignment grades
+        // if (assignments.length > 0) {
+        //   this.studentsEnrolled.forEach(student => {
+        //     assignments.forEach(assignment => {
+        //       this.fetchAssignmentGrade(course, lesson, student.email, assignment.assignment_name);
+        //     });
+        //   });
+        // }
       },
       error: (error) => {
         console.error('Error fetching assignments', error);
@@ -142,26 +148,41 @@ export class TeacherDashboardCourseComponent implements OnInit {
     });
   }
 
-  fetchAssignmentGrade(course: string, lesson: string, email: string, assignmentName: string): void {
-    this.assignmentService.getStudentGrade(course, lesson, email).subscribe({
-      next: (grades: number[]) => {
-        const grade = grades[0] || null;
-        this.assignmentGrades[email + assignmentName] = {
-          status: grade !== null ? 'Graded' : 'Ungraded',
-          grade: grade !== null ? grade : 0
-        };
-      },
-      error: (error) => {
-        console.error('Error fetching assignment grades', error);
-      }
-    });
+  // fetchAssignmentGrade(course: string, lesson: string, email: string, assignmentName: string): void {
+  //   this.assignmentService.getStudentGrade(course, lesson, email).subscribe({
+  //     next: (grades: number[]) => {
+  //       const grade = grades[0] || null;
+  //       this.assignmentGrades[email + assignmentName] = {
+  //         status: grade !== null ? 'Graded' : 'Ungraded',
+  //         grade: grade !== null ? grade : 0
+  //       };
+  //     },
+  //     error: (error) => {
+  //       console.error('Error fetching assignment grades', error);
+  //     }
+  //   });
+  // }
+
+  // getAssignmentGrade(email: string, assignmentName: string): number {
+  //   //return this.assignmentGrades[email + assignmentName]?.grade || 0;
+  //   return this.assignments?.grade || 0;
+  // }
+  getAssignmentGrade(ass: AllAssignments): number {
+    //return this.assignmentGrades[email + assignmentName]?.grade || 0;
+    return ass?.grade !== null ? ass?.grade : 0;
   }
 
-  getAssignmentGrade(email: string, assignmentName: string): number {
-    return this.assignmentGrades[email + assignmentName]?.grade || 0;
+  // getAssignmentStatus(email: string, assignmentName: string): string {
+  //   return this.assignmentGrades[email + assignmentName]?.status || 'Ungraded';
+  // }
+  getAssignmentStatus(ass: AllAssignments): string {
+    const status =  ass?.grade !== null && ass?.grade !== 0;
+    return status ? 'Graded' : 'Ungraded';
   }
 
-  getAssignmentStatus(email: string, assignmentName: string): string {
-    return this.assignmentGrades[email + assignmentName]?.status || 'Ungraded';
+  gradeAssignment(student: Student)
+  {
+    console.log(student)
+    this.router.navigateByUrl(`course/${this.selectedCourse}/lesson/${this.selectedLesson}/grade?student=${student.email}`)
   }
 }
